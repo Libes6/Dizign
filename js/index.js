daf8fa5c2082ed025c3ca7cfe45b7bf9d80bbb05
Max <88722440+Libes6@users.noreply.github.com>
2022-10-23 16:00:36 +0700

    burger

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log("hi1")
+const burgerBtn= document.querySelector('.menu__icon')
+const burger= document.querySelector('.menu__list')
+
+
+
+
+if(burgerBtn && burger){
+    burgerBtn.addEventListener('click',()=>{
+        burgerBtn.classList.toggle('active')
+        burger.classList.toggle('active')
+    })
+}
